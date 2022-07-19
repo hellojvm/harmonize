93ec3e5cff713e2de5f1d488eb6fac889f6b78d1
jony.fu <jony.fu@mymro.cn>
2022-07-19 16:22:35 +0800

    test

--- src/main/java/com/fd/web/ws/RestServer.java
+++ src/main/java/com/fd/web/ws/RestServer.java
@@ -107,7 +107,7 @@ public class RestServer {
 							ite.remove();
 							ClientApi clientApi = disapicl.getClientApi();
 							clientApi.getHttpApiInfo().setIsOnline(false);
-//							sendapi(clientApi, session);///TODO 
+							sendapi(clientApi, session);
 							log.error(String.format("%s客户端销毁成功..", session.getId()));
 							log.error("还剩客户端总数量:{}", CoordinateUtil.CLIENTS.size());
 						}
@@ -214,7 +214,7 @@ public class RestServer {
 				e.printStackTrace();
 				log.error("sendping:", e);
 			}
-		}, 13, 9, TimeUnit.SECONDS);
+		}, 13, 39, TimeUnit.SECONDS);
 
 	}
 
